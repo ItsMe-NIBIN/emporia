0568a8e58f84322a8313099d929815275dc9e702
nibin <nibinkuriannk@gmail.com>
2023-08-16 23:11:38 +0530

    project detail page responsive pending

--- projectDetails.php
+++ projectDetails.php
@@ -17,11 +17,96 @@
     <!-- Inner Banner Section  -->
 
 
-    <!-- projects Listing Section  -->
-    
-    <!-- projects Listing Section  -->
+    <!-- projects Details Section  -->
+    <section id="projectDetailSec">
+        <div class="container">
+            <div class="contentSec">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quo modo autem optimum, si bonum praeterea
+                    nullum est? Tum ille: Tu autem cum ipse tantum librorum habeas, quos hic tandem requiris? Quo studio
+                    Aristophanem putamus aetatem in litteris duxisse? Quod mihi quidem visus est, cum sciret, velle
+                    tamen confitentem audire Torquatum.
+                </p>
+                <p>Qui-vere falsone, quaerere mittimus-dicitur oculis se privasse; Hoc est dicere: Non reprehenderem
+                    asotos, si non essent asoti. Atque haec coniunctio confusioque virtutum tamen a philosophis ratione
+                    quadam distinguitur. Mihi vero, inquit, placet agi subtilius et</p>
+            </div>
+            <div class="detailSec">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <a href="assets/images/projDet1.jpg" class="proDetBx" data-fancybox="group">
+                            <img src="assets/images/projDet1.jpg" alt="Projects" width="1494" height="637" class="lazy"
+                                loading="lazy" data-src="assets/images/projDet1.jpg">
+                        </a>
+                    </div>
+                    <div class="col-lg-6">
+                        <a href="assets/images/projDet2.jpg" class="proDetBx" data-fancybox="group">
+                            <img src="assets/images/projDet2.jpg" alt="Projects" width="1494" height="637" class="lazy"
+                                loading="lazy" data-src="assets/images/projDet2.jpg">
+                        </a>
+                    </div>
+                    <div class="col-lg-6">
+                        <a href="assets/images/projDet3.jpg" class="proDetBx" data-fancybox="group">
+                            <img src="assets/images/projDet3.jpg" alt="Projects" width="1494" height="637" class="lazy"
+                                loading="lazy" data-src="assets/images/projDet3.jpg">
+                        </a>
+                    </div>
+                    <div class="col-lg-12">
+                        <a href="assets/images/projDet4.jpg" class="proDetBx" data-fancybox="group">
+                            <img src="assets/images/projDet4.jpg" alt="Projects" width="1494" height="637" class="lazy"
+                                loading="lazy" data-src="assets/images/projDet4.jpg">
+                        </a>
+                    </div>
+                    <div class="col-lg-6">
+                        <a href="assets/images/projDet5.jpg" class="proDetBx" data-fancybox="group">
+                            <img src="assets/images/projDet5.jpg" alt="Projects" width="1494" height="637" class="lazy"
+                                loading="lazy" data-src="assets/images/projDet5.jpg">
+                        </a>
+                    </div>
+                    <div class="col-lg-6">
+                        <a href="assets/images/projDet6.jpg" class="proDetBx" data-fancybox="group">
+                            <img src="assets/images/projDet6.jpg" alt="Projects" width="1494" height="637" class="lazy"
+                                loading="lazy" data-src="assets/images/projDet6.jpg">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="pagination">
+                <div class="row">
+                    <div class="col-md-6">
+                        <a href="javascript:void(0);" class="paginateBx prev">
+                            <div class="imgSec">
+                                <img src="assets/images/paginate1.jpg" alt="">
+                            </div>
+                            <div class="cntSec">
+                                <div class="dir">Previous Project</div>
+                                <div class="name">Emirates Palace</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-6">
+                        <a href="javascript:void(0);" class="paginateBx next">
+                            <div class="imgSec">
+                                <img src="assets/images/paginate2.jpg" alt="">
+                            </div>
+                            <div class="cntSec">
+                                <div class="dir">Next Project</div>
+                                <div class="name">Le Meridien</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- projects Details Section  -->
 
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css"
+        integrity="sha512-H9jrZiiopUdsLpg94A333EfumgUBpO9MdbxStdeITo+KEIMaNfHNvwyjjDJb+ERPaRS6DpyRlKbvPUasNItRyw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"
+        integrity="sha512-uURl+ZXMBrF4AwGaWmEetzrd+J5/8NRkWAvJx5sbPSSuOb0bZLqf+tOzniObO00BjHa/dD7gub9oCGMLPQHtQA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        
 
 </div>
 
--- projects.php
+++ projects.php
@@ -18,7 +18,102 @@
 
 
     <!-- projects Listing Section  -->
-    
+    <section id="projectsListingSec">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-sm-6">
+                    <a href="projectDetails.php" class="projBx">
+                        <img src="assets/images/project1.png" alt="Project" class="lazy" loading="lazy"
+                            data-src="assets/images/project1.png">
+                        <div class="cntSec">
+                            <div class="title">ST. REGIS HOTEL</div>
+                            <div class="location">United Arab Emirates</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="projectDetails.php" class="projBx">
+                        <img src="assets/images/project2.png" alt="Project" class="lazy" loading="lazy"
+                            data-src="assets/images/project2.png">
+                        <div class="cntSec">
+                            <div class="title">ST. REGIS HOTEL</div>
+                            <div class="location">United Arab Emirates</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="projectDetails.php" class="projBx">
+                        <img src="assets/images/project3.png" alt="Project" class="lazy" loading="lazy"
+                            data-src="assets/images/project3.png">
+                        <div class="cntSec">
+                            <div class="title">ST. REGIS HOTEL</div>
+                            <div class="location">United Arab Emirates</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="projectDetails.php" class="projBx">
+                        <img src="assets/images/project4.png" alt="Project" class="lazy" loading="lazy"
+                            data-src="assets/images/project4.png">
+                        <div class="cntSec">
+                            <div class="title">ST. REGIS HOTEL</div>
+                            <div class="location">United Arab Emirates</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="projectDetails.php" class="projBx">
+                        <img src="assets/images/project5.png" alt="Project" class="lazy" loading="lazy"
+                            data-src="assets/images/project5.png">
+                        <div class="cntSec">
+                            <div class="title">ST. REGIS HOTEL</div>
+                            <div class="location">United Arab Emirates</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="projectDetails.php" class="projBx">
+                        <img src="assets/images/project6.png" alt="Project" class="lazy" loading="lazy"
+                            data-src="assets/images/project6.png">
+                        <div class="cntSec">
+                            <div class="title">ST. REGIS HOTEL</div>
+                            <div class="location">United Arab Emirates</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="projectDetails.php" class="projBx">
+                        <img src="assets/images/project1.png" alt="Project" class="lazy" loading="lazy"
+                            data-src="assets/images/project1.png">
+                        <div class="cntSec">
+                            <div class="title">ST. REGIS HOTEL</div>
+                            <div class="location">United Arab Emirates</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="projectDetails.php" class="projBx">
+                        <img src="assets/images/project2.png" alt="Project" class="lazy" loading="lazy"
+                            data-src="assets/images/project2.png">
+                        <div class="cntSec">
+                            <div class="title">ST. REGIS HOTEL</div>
+                            <div class="location">United Arab Emirates</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="projectDetails.php" class="projBx">
+                        <img src="assets/images/project3.png" alt="Project" class="lazy" loading="lazy"
+                            data-src="assets/images/project3.png">
+                        <div class="cntSec">
+                            <div class="title">ST. REGIS HOTEL</div>
+                            <div class="location">United Arab Emirates</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
     <!-- projects Listing Section  -->
 
 
